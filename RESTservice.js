0fc120f88ecd8e850935b0d88479bf9ed90b65ff
gururajb66 <gururajb66@gmail.com>
2018-10-24 19:32:13 -0400

    Update RESTservice.js

--- RESTservice.js
+++ RESTservice.js
@@ -1,89 +1,27 @@
-var exp=require('express');
-var fs=require('fs');
-var bodyparser=require('body-parser');
-var app=exp();
-var urlencodedparser  = bodyparser.urlencoded({extended:true});
-app.get('/RESTservice.htm',function(r,s){
-	console.log("Got a request");
-	s.sendFile(__dirname+'/RESTservice.htm');
-	});
-app.get('/get_emp',function(r,s){
-		s.header({"Content-type":"json/text"});
-	console.log(r.query);
-	fs.readFile(__dirname+'/DBObj.txt',function(err,data){
-		var d = JSON.parse(data);
-		var emppresent=false;
-		d.Employee.forEach(function(item){
-			if(item.Id===r.query.id)
-			{
-				emppresent=true;
-				s.end(JSON.stringify(item));
-			}
-		});
-		if(!emppresent)
-			s.end("Employee not found");
-
-		});
-	});
-
-app.post('/update_emp',urlencodedparser,function(r,s){
-	fs.readFile(__dirname+'/DBObj.txt',function(err,data){
-		var d= JSON.parse(data);
-		d.Employee.forEach(function(item){
-			console.log(r.body.id);
-			if(item.Id===r.body.id)
-			{
-				item.Name=r.body.name;
-				item.Dept=r.body.dept;
-			}
-			});
-			console.log(JSON.stringify(d));
-			fs.writeFile(__dirname+'/DBObj.txt',JSON.stringify(d),function(err,stat){
-				s.end("Updated");
-				});
-
-		});
-
-
-	});
-
-app.post('/add_emp',urlencodedparser,function(r,s){
-	fs.readFile(__dirname+'/DBObj.txt',function(err,data){
-		var d= JSON.parse(data);
-		var emppresent=false
-		d.Employee.forEach(function(item){
-			console.log(r.body.id);
-			if(item.Id===r.body.id)
-			{
-				emppresent=true;
-				s.end("Employee with that Id already present");
-			}
-			});
-			if(!emppresent)
-			{
-			d.Employee.push({"Id":r.body.id,
-			"Name":r.body.name,
-			"Dept":r.body.dept
-			});
-			console.log(JSON.stringify(d));
-			fs.writeFile(__dirname+'/DBObj.txt',JSON.stringify(d),function(err,stat){
-			s.end("Added");
-			});
-			}
-
-
-		});
-
-
-	});
-
-
-
-const server = app.listen(process.env.PORT||"8080",function(){
-	const port = server.address().port;
-	console.log("Emp App is listening on "+port);
-	});
-
-//app.post();
-
-//app.put();t
+var nforce = require('nforce');
+// create the connection with the Salesforce connected app
+var org = nforce.createConnection({
+  clientId: 'SOME_OAUTH_CLIENT_ID',
+  clientSecret: 'SOME_OAUTH_CLIENT_SECRET',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  mode: 'single'
+});
+// authenticate and return OAuth token
+org.authenticate({
+  username: gururaj4cloud@gmail.com,
+  password: 'S2lesforce'+''
+}, function(err, resp){
+  if (!err) {
+    console.log('Successfully logged in! Cached Token: ' + org.oauth.access_token);
+    // execute the query
+    org.query({ query: 'select id, name from account limit 5' }, function(err, resp){
+      if(!err && resp.records) {
+        // output the account names
+        for (i=0; i<resp.records.length;i++) {
+          console.log(resp.records[i].get('name'));
+        }
+      }
+    });
+  }
+  if (err) console.log(err);
+});
